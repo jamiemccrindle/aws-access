fc9af33e3da6b720ff403220c4ed5f86a7e26fdb
Jamie McCrindle <jamiemccrindle@gmail.com>
2016-08-07 14:40:18 +0100

    only request ip once

--- index.js
+++ index.js
@@ -22,68 +22,68 @@ var options = argv.profile ? {profile: argv.profile} : {};
 var credentials = new AWS.SharedIniFileCredentials(options);
 AWS.config.credentials = credentials;
 
-Promise.map(argv.regions).then(function(region) {
-  AWS.config.update({region: region});
+request('https://wtfismyip.com/text').then(function (result) {
+  var ip = result.trim();
+  console.log('using ip ' + ip);
+  return Promise.each(argv.regions, function (region) {
+    AWS.config.update({region: region});
 
-  var ec2 = Promise.promisifyAll(new AWS.EC2());
+    var ec2 = Promise.promisifyAll(new AWS.EC2());
 
-  var ip;
-  var groupId;
+    var groupId;
 
-  return request('https://wtfismyip.com/text')
-    .then(function (result) {
-      ip = result.trim();
-      console.log('using ip ' + ip);
-      return ec2.describeSecurityGroupsAsync({
-        GroupNames: [argv.group]
-      })
+    return ec2.describeSecurityGroupsAsync({
+      GroupNames: [argv.group]
     }).then(function (data) {
-    if (!data['SecurityGroups']) {
-      return Promise.reject("No security groups found with name " + argv.group)
-    }
-    if (data['SecurityGroups'].length != 1) {
-      return Promise.reject("Should only be 1 security group but was " + data['SecurityGroups'].length)
-    }
-    var group = data['SecurityGroups'][0];
-    groupId = group.GroupId;
-    console.log('found group ' + groupId);
-    var ipPermissions = group.IpPermissions.map(function(permission) {
-      var result = {};
-      Object.keys(permission).forEach(function(key) {
-        if(permission[key]) {
-          if(Array.isArray(permission[key])) {
-            if(permission[key].length > 0) {
+      if (!data['SecurityGroups']) {
+        return Promise.reject('No security groups found with name ' + argv.group)
+      }
+      if (data['SecurityGroups'].length != 1) {
+        return Promise.reject("Should only be 1 security group but was " + data['SecurityGroups'].length)
+      }
+      var group = data['SecurityGroups'][0];
+      groupId = group.GroupId;
+      console.log(region + ' found group ' + groupId);
+      var ipPermissions = group.IpPermissions.map(function (permission) {
+        var result = {};
+        Object.keys(permission).forEach(function (key) {
+          if (permission[key]) {
+            if (Array.isArray(permission[key])) {
+              if (permission[key].length > 0) {
+                result[key] = permission[key];
+              }
+            } else {
               result[key] = permission[key];
             }
-          } else {
-            result[key] = permission[key];
           }
-        }
+        });
+        return result;
       });
-      return result;
-    });
-    return ec2.revokeSecurityGroupIngressAsync({
-      GroupId: groupId,
-      IpPermissions: ipPermissions
-    })
-  }).then(function () {
-    return ec2.authorizeSecurityGroupIngressAsync({
-      GroupId: groupId,
-      IpPermissions: argv.ports.map(function (port) {
-        var p = parseInt(port);
-        return {
-          IpRanges: [
-            {CidrIp: ip + '/32'}
-          ],
-          FromPort: p,
-          ToPort: p,
-          IpProtocol: 'tcp'
-        }
+      return ec2.revokeSecurityGroupIngressAsync({
+        GroupId: groupId,
+        IpPermissions: ipPermissions
       })
+    }).then(function () {
+      return ec2.authorizeSecurityGroupIngressAsync({
+        GroupId: groupId,
+        IpPermissions: argv.ports.map(function (port) {
+          var p = parseInt(port);
+          return {
+            IpRanges: [
+              {CidrIp: ip + '/32'}
+            ],
+            FromPort: p,
+            ToPort: p,
+            IpProtocol: 'tcp'
+          }
+        })
+      });
+    }).then(function () {
+      console.log(region + ' updated group ' + groupId + ' to ip ' + ip + '/32 and ports ' + argv.ports);
     });
-  })
+  });
 }).then(function () {
-  console.log('updated group ' + groupId + ' to ip ' + ip + '/32 and ports ' + argv.ports);
+  console.log('done');
   process.exit(0);
 }).error(function (error) {
   console.log(error.stack);
